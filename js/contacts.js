5d12b8b998aee9d72cceca91ba84677a1bdd2d43
DimGit <dimgit@posteo.net>
2018-11-05 11:57:41 +0100

    Modification de contacts.js avec la correction proposée : boucle avec while, et choix avec switch

--- js/contacts.js
+++ js/contacts.js
@@ -2,59 +2,56 @@
 Activité : gestion des contacts
 */
 
-//Création du modèle de contact
 var Contact = {
-	//Initialisation du contact
-	init: function (nom, prenom) {
-		this.nom = nom;
-		this.prenom = prenom;
-	},
-	//Décrire le contact
-	decrire: function () {
-		var description = "Nom : " + this.nom + ", prénom : " + this.prenom;
-		return description;
-	}
+    // initialise les attributs du contact
+    init: function (nom, prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    },
+    // Renvoie la description du contact
+    decrire: function () {
+        return "Nom : " + this.nom + ", prénom : " + this.prenom;
+    }
 };
 
-var contact1 = Object.create(Contact);
-contact1.init("Lévisse", "Carole");
+var carole = Object.create(Contact);
+carole.init("Lévisse", "Carole");
+var melodie = Object.create(Contact);
+melodie.init("Nelsonne", "Mélodie");
 
-var contact2 = Object.create(Contact);
-contact2.init("Nelsonne", "Mélodie");
-
-//Création du tableau
 var contacts = [];
+contacts.push(carole);
+contacts.push(melodie);
 
-contacts.push(contact1);
-contacts.push(contact2);
+console.log("Bienvenue dans le gestionnaire des contacts !");
+var choix;
+while (choix !== "0") {
+    console.log("1 : Lister les contacts");
+    console.log("2 : Ajouter un contact");
+    console.log("0 : Quitter");
+    var choix = prompt("Choisissez une option :");
 
-//Lancement du programme
-console.log("Bienvenue dans le gestionnaire de contact");
-
-for (var option = ""; option !== "0";) {
-	console.log(" \n1 : Lister les contacts");
-	console.log("2 : Ajouter un contact");
-	console.log("0 : Quitter\n ");
-	option = prompt("Choisissez une option :");
-
-	if (option === "1") {
-			console.log(" \nVoici la liste de tous vos contacts");
-			contacts.forEach(function (contacts) {
-			console.log(contacts.decrire());
-		});
-	}
-		else if (option === "2") {
-				var nom = prompt("Entrez le nom du nouveau contact :");
-				var prenom = prompt("Entrez le prénom du nouveau contact:");
-				var newContact = ("contact" + Number(contacts.length + 1));
-
-				newContact = Object.create(Contact);
-				newContact.init(nom, prenom);
-				contacts.push(newContact);
-
-				console.log("Le nouveau contact a été ajouté");
-		}
-};
-
-//Fin du programme
-console.log("Au revoir!\n ");
+    switch (choix) {
+    case "1":
+        console.log("Voici la liste de tous vos contacts :");
+        // Avec une boucle for
+        for (var i = 0; i < contacts.length; i++) {
+            console.log(contacts[i].decrire());
+        }
+        // Avec une boucle foreach
+        /*contacts.forEach(function (contact) {
+            console.log(contact.decrire());
+        });*/
+        break;
+    case "2":
+        var nom = prompt("Entrez le nom du nouveau contact :");
+        var prenom = prompt("Entrez le prénom du nouveau contact :");
+        var contact = Object.create(Contact);
+        contact.init(nom, prenom);
+        contacts.push(contact);
+        console.log("Le nouveau contact a été ajouté");
+        break;
+    }
+    console.log(); // Passe une ligne
+}
+console.log("Au revoir !");
